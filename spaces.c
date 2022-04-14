ebb3d68febf0dd24efcee81ea20bd2e11247a3ad
FranQ <frankchristopherquispe@gmail.com>
2022-04-14 00:05:05 +0000

    correccion: redimension del malloc ahora incluye el caracter nulo

--- spaces.c
+++ spaces.c
@@ -5,7 +5,7 @@ char *spaces(char *input)
 	int i = 0, j = 0;
 	char *result = NULL;
 
-	result = malloc(sizeof(char) * _strlen(input));
+	result = malloc(sizeof(char) * (_strlen(input) + 1));
 	if (result == NULL)
 		return(NULL);
 
