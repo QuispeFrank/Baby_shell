63f024d3e9830f9592b09a244fbad787343ff0c0
LesLor <leslie.paz.am@gmail.com>
2022-04-15 20:48:33 +0000

    New changes

--- main.c
+++ main.c
@@ -1,35 +1,5 @@
 #include "main.h"
 
-void _execve(char **argVec)
-{
-	pid_t pid = 0;
-	int status = 0;
-
-	/* el programa se divide en padre e hijo */
-	pid = fork();
-
-	if (pid == -1)
-	{
-		exit(1);
-	}
-	else if (pid == 0)
-	{
-		/* child */
-		execve(argVec[0], argVec, NULL);
-		_free(argVec);
-		free_env();
-		exit(0);
-	}
-	else
-	{
-		/* father */
-		wait(&status);
-		_free(argVec);
-	}	
-}
-
-
-
 int main(void)
 {
 	char *prompt = "$ ", *cmd = NULL, **av = NULL;
