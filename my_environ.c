b3ba7d56d19518e63e3df0dfbbd58a20039942a1
FranQ <frankchristopherquispe@gmail.com>
2022-04-19 01:37:54 +0000

    en my_environ.c se redimensiona la funcion copy_env y se agregan 3 nuevas: already_exist_then_delete, douptr_cpy, dupdou_pointer_from - valgring ok

--- my_environ.c
+++ my_environ.c
@@ -1,10 +1,72 @@
 #include "main.h"
 
 /**
+ * already_exist_then_replace - verifica si existe una variable de entorno.
+ * @av: double pointer who contain the name to find.
  *
+ * Description: la funcion verifica @av contenga un nombre valido que se 
+ * encuentre dentro del double pointer environ, si encuentra reemplaza 
+ * su valor(usa malloc) y libera la memoria del antiguo valor, si falla
+ * devuelve -2, si no encuentra la variable de entorno devuelve -1.
  *
+ * Return: -2 if it fails, -1 if the variable env doesnt exist, 0 in success.
  */
+int already_exist_then_replace(char **av)
+{
+	char *env_aux = NULL;
+	int r_index = 0;
 
+	r_index = _getenv_index(av[1]);
+	if (r_index != -1)
+	{
+		/* existe, guardamos contenido original por si falla algo */
+		env_aux = environ[r_index];
+		/* reemplazo del antiguo bloque */ 
+		environ[r_index] = _getenv_format(av);
+		if (environ[r_index] == NULL)
+		{
+			environ[r_index] = env_aux;
+			return(-2);
+		}
+		free(env_aux);
+		reinit_environ(_dupdou_pointer_from(environ));
+		return(0);
+	}
+	return (-1);
+}
+
+
+/**
+ * douptr_cpy - function that copy a double pointer.
+ * @src: a double pointer where from stare copying.
+ * @dest: double pointer where to store the copy.
+ *
+ * Description: esta funcion hace una copia desde el double 
+ * pointer @src hacia el double pointer @dest, para lograrlo
+ * el puntero destino debe contener el espacio necesario o mas,
+ * esta funcion hace uso de malloc, si la copia falla libera
+ * el double puntero @dest.
+ *
+ * Return: NULL si falla, otherwise @dest.
+ */
+char **douptr_cpy(char **dest, char **src)
+{
+	int index = 0;
+	while (src[index])
+        {
+		/* duplicando una variable de entorno en ptr */ 
+		dest[index] = _strdup(src[index]);
+		if (dest[index] == NULL)
+		{
+			_free(dest);
+			return(NULL);
+		}
+		index++;
+	}
+	dest[index] = NULL;
+
+	return(dest);
+}
 
 /**
  * _dupdou_pointer_from - a function that duplicates a double pointer.
@@ -18,30 +80,21 @@
 char **_dupdou_pointer_from(char **src)
 {
 	char **dest = NULL;
-	int len, index = 0;
+	int len;
 
 	/* tamaño del source */
         for (len = 0; src[len]; len++)
-                ;
+		;
 
 	/* espacio para los punteros */
         dest = malloc(sizeof(char *) * (len + 1));
         if (dest == NULL)
                 return (NULL);
 
-	/* comienza la copia desde src hacia dest */
-	while (src[index])
-        {
-		/* duplicando una variable de entorno en ptr */ 
-		dest[index] = _strdup(src[index]);
-		if (dest[index] == NULL)
-		{
-			_free(dest);
-			return(NULL);
-		}
-		index++;
-	}
-	dest[index] = NULL;
+	/* se duplica desde src hacia dest */
+	dest = douptr_cpy(dest, src);
+	if (dest == NULL)
+		return (NULL);
 
 	return (dest);
 }
